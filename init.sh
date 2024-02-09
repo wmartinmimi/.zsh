9da4784cd8e2530a7e438a4ff1f39fdd8ea31f73
wmartinmimi <wmartinmimipub@gmail.com>
2024-02-09 17:39:55 +0000

    update autocompletes

--- init.sh
+++ init.sh
@@ -3,3 +3,7 @@ source ~/.zsh/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
 source ~/.zsh/zsh-autocomplete/zsh-autocomplete.plugin.zsh
 source ~/.zsh/zsh-colored-man-pages/colored-man-pages.plugin.zsh
 
+zstyle ':completion:*' completer _extensions _complete _approximate _history
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
