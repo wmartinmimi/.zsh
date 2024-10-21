0ea58ba4a6cec1926c54840d2dc33d865445e8ee
wmartinmimi <wmartinmimipub@gmail.com>
2024-10-21 18:53:30 +0100

    removed unrequired autocomplete

--- init.sh
+++ init.sh
@@ -1,10 +1,19 @@
 source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 source ~/.zsh/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
-source ~/.zsh/zsh-autocomplete/zsh-autocomplete.plugin.zsh
 source ~/.zsh/zsh-colored-man-pages/colored-man-pages.plugin.zsh
 
 zstyle ':completion:*' completer _extensions _complete _approximate _history
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-zstyle ':autocomplete:*:*' list-lines 10
+zstyle ':completion:*' menu select
+zstyle ':completion:*:*:*:*:descriptions' format '%F{green}-- %d --%f'
+zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
+zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
+zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' complete-options true
+
+bindkey '\t' menu-complete
+
+export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 
