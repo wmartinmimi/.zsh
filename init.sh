18571aaf813fbed37f216c168648feaacbfe1edd
wmartinmimi <wmartinmimipub@gmail.com>
2024-10-21 19:29:06 +0100

    add fzf-tab

--- init.sh
+++ init.sh
@@ -1,19 +1,27 @@
-source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
-source ~/.zsh/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
-source ~/.zsh/zsh-colored-man-pages/colored-man-pages.plugin.zsh
-
 zstyle ':completion:*' completer _extensions _complete _approximate _history
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-zstyle ':completion:*' menu select
-zstyle ':completion:*:*:*:*:descriptions' format '%F{green}-- %d --%f'
-zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
-zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
-zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
+zstyle ':completion:*' menu no
+zstyle ':completion:*:*:*:*:descriptions' format '-- %d --'
+zstyle ':completion:*:descriptions' format '[%d]'
+zstyle ':completion:*:*:*:*:corrections' format '!- %d (errors: %e) -!'
+zstyle ':completion:*:messages' format '-- %d --'
+zstyle ':completion:*:warnings' format '-- no matches found --'
 zstyle ':completion:*' group-name ''
 zstyle ':completion:*' complete-options true
+# zstyle ':autocomplete:*:*' list-lines 16
 
-bindkey '\t' menu-complete
+# zstyle ':fzf-tab:*' use-fzf-default-opts yes
+zstyle ':fzf-tab:*' fzf-flags --color=fg:1,fg+:2 --bind=tab:accept --bind=enter:accept --height=16
+# switch group using `<` and `>`
+zstyle ':fzf-tab:*' switch-group 'left' 'right'
+
+autoload -U compinit; compinit
+source ~/.zsh/fzf-tab/fzf-tab.plugin.zsh
+
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ~/.zsh/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+source ~/.zsh/zsh-colored-man-pages/colored-man-pages.plugin.zsh
 
 export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 
