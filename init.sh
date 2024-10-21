00cfd3d7ef65ce88035e27726476d7e6bcc49331
wmartinmimi <wmartinmimipub@gmail.com>
2024-10-21 18:05:29 +0100

    up

--- init.sh
+++ init.sh
@@ -6,4 +6,5 @@ source ~/.zsh/zsh-colored-man-pages/colored-man-pages.plugin.zsh
 zstyle ':completion:*' completer _extensions _complete _approximate _history
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+zstyle ':autocomplete:*:*' list-lines 10
 
